2d47ffc9499e59bd322c0eead653633789ecd226
vhraposo <thevhup@gmail.com>
2024-01-16 00:10:09 -0300

    Enviando dados do formulário para controller

--- src/main/java/br/com/vraposo/front_gestao_vagas/PrimeiraPaginaController.java
+++ src/main/java/br/com/vraposo/front_gestao_vagas/PrimeiraPaginaController.java
@@ -3,6 +3,7 @@ package br.com.vraposo.front_gestao_vagas;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +20,11 @@ public class PrimeiraPaginaController {
     @GetMapping("/login")
     public String loginCandidate(){
         return "candidate/login";
+    }
+
+    @PostMapping("/create")
+    public String cadastroCandidate(String nome_do_candidato){
+        System.out.println("Nome do candidato: " + nome_do_candidato);
+        return "/candidate/login";
     }
 }
