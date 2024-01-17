ba2b67ea5795ace98528827caa83edde794b60b2
vhraposo <thevhup@gmail.com>
2024-01-17 00:57:23 -0300

    Criando página do perfil

--- src/main/java/br/com/vraposo/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/vraposo/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -37,4 +37,10 @@ public class CandidateController {
             return "redirect:/candidate/login";
         }
     }
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "candidate/profile";
+    }
+
 }
