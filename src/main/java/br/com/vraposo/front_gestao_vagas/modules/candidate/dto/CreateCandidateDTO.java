47373b62d4765d57f3faf697d37205f3c5001346
vhraposo <thevhup@gmail.com>
2024-03-04 10:01:34 -0300

    Enviando form controller

---
+++ src/main/java/br/com/vraposo/front_gestao_vagas/modules/candidate/dto/CreateCandidateDTO.java
@@ -0,0 +1,13 @@
+package br.com.vraposo.front_gestao_vagas.modules.candidate.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateCandidateDTO {
+    
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String description;
+}
