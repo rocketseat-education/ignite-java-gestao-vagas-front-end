1bfb6357eda76455348dc6bbb3f49867bb400283
vhraposo <thevhup@gmail.com>
2024-03-12 10:16:15 -0300

    Criando tela

--- src/main/java/br/com/vraposo/front_gestao_vagas/modules/company/CompanyController.java
+++ src/main/java/br/com/vraposo/front_gestao_vagas/modules/company/CompanyController.java
@@ -1,6 +1,7 @@
 package br.com.vraposo.front_gestao_vagas.modules.company;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
@@ -77,4 +78,11 @@ public class CompanyController {
             return "redirect:/company/jobs";
         }
     }
+
+    @GetMapping("/jobs")
+    @PreAuthorize("hasRoles('COMPANY')")
+    public String jobs(){
+        return "company/jobs";
+    }
+
 }
