a96cd741a21e017f7f216d5454918e0f296275f1
vhraposo <thevhup@gmail.com>
2024-03-15 01:16:15 -0300

    Criando página de listagem

--- src/main/java/br/com/vraposo/front_gestao_vagas/modules/company/CompanyController.java
+++ src/main/java/br/com/vraposo/front_gestao_vagas/modules/company/CompanyController.java
@@ -99,6 +99,13 @@ public class CompanyController {
         return "redirect:/company/jobs";
     }
 
+    @GetMapping("/jobs/list")
+    @PreAuthorize("hasRole('COMPANY')")
+    public String list(Model model){
+        //model.addAttribute("jobs", new CreateJobsDTO());
+        return "company/list";
+    }
+
     private String getToken(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getDetails().toString();
