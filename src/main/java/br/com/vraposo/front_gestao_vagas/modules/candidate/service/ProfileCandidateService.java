6016ce4bd2de5b586ead5b357a57ac33ccc00ae8
vhraposo <thevhup@gmail.com>
2024-03-17 20:04:10 -0300

    Criando properties url

--- src/main/java/br/com/vraposo/front_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
+++ src/main/java/br/com/vraposo/front_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
@@ -2,6 +2,7 @@ package br.com.vraposo.front_gestao_vagas.modules.candidate.service;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -17,15 +18,18 @@ import br.com.vraposo.front_gestao_vagas.modules.candidate.dto.ProfileUserDTO;
 @Service
 public class ProfileCandidateService {
 
+    @Value("${host.api.gestao.vagas}")
+    private String hostAPIGestaoVagas;
+
     public ProfileUserDTO execute(String token) {
         RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
 
         HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
-
+        var url = hostAPIGestaoVagas.concat("/candidate/");
         try{
-            var result = rt.exchange("http://localhost:8080/candidate/", HttpMethod.GET, request, ProfileUserDTO.class);
+            var result = rt.exchange(url, HttpMethod.GET, request, ProfileUserDTO.class);
             System.out.println(result);
             return result.getBody();
         }catch(Unauthorized ex){
